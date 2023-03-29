7835d51b31127f4dcd4088cd0578f7255a8cd29a
schrattenholz <46528228+schrattenholz@users.noreply.github.com>
2023-03-29 08:59:53 +0200

    Update OrderConfig.php

--- src/Backend/OrderConfig.php
+++ src/Backend/OrderConfig.php
@@ -53,4 +53,23 @@ class OrderConfig extends DataObject
 	];
 
 }
+	public function canView($member = null) 
+    {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canEdit($member = null) 
+    {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canDelete($member = null) 
+    {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canCreate($member = null, $context = []) 
+    {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
 ?>
